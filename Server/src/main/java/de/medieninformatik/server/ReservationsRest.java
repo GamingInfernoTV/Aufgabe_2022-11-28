f952f3955f4072462e772b5c1347b3f093f97a65
aaron <aaronpoehlmann1310@gmail.com>
2022-11-29 13:20:11 +0100

    Version 0.3: ReservationsRest responses and build Gradle implementation

--- Server/src/main/java/de/medieninformatik/server/ReservationsRest.java
+++ Server/src/main/java/de/medieninformatik/server/ReservationsRest.java
@@ -1,5 +1,7 @@
 package de.medieninformatik.server;
 
+import de.medieninformatik.common.InvalidSeatException;
+import de.medieninformatik.common.Seat;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
@@ -9,8 +11,13 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.util.Optional;
+
 @Path("reservation")
 public class ReservationsRest {
+
+    ReservationsImpl reservation = new ReservationsImpl();
+    Response response;
 
     @GET
     @Path("{row}/{num}")
@@ -18,7 +25,20 @@ public class ReservationsRest {
     public Response getReservation(
             @PathParam("row") int row,
             @PathParam("num") int num) {
-        return null; // TODO: Create response
+        System.out.println("GET " + row + ", " + num);
+        Seat seat = new Seat(row, num);
+        try {
+            Optional<String> optional = reservation.getReservation(seat);
+            if (optional.isPresent()) {
+                response = Response.ok(seat.toString()).build();
+            } else {
+                response = Response.noContent().build();
+            }
+        } catch (InvalidSeatException e) {
+            response = Response.status(Response.Status.FORBIDDEN).build();
+        }
+        return response;
+        // TODO: Create response
     }
 
     @GET
@@ -27,9 +47,16 @@ public class ReservationsRest {
     public Response hasReservation(
             @PathParam("row") int row,
             @PathParam("num") int num) {
-        return null; // TODO: Create response
+        Seat seat = new Seat(row, num);
+        try {
+            response = Response.ok(reservation.hasReservation(seat)).build();
+        } catch (InvalidSeatException e) {
+            response = Response.status(Response.Status.NO_CONTENT).build();
+        }
+        return response; // TODO: Create response
     }
 
+    // PUT, wenn Werte bearbeitet werden POST, wenn Werte neu erstellt werden
     @POST
     @Path("{row}/{num}/{name}")
     @Consumes(MediaType.TEXT_PLAIN)
@@ -37,6 +64,16 @@ public class ReservationsRest {
             @PathParam("row") int row,
             @PathParam("num") int num,
             @PathParam("name") String name) {
-        return null; // TODO: Create response
+        Seat seat = new Seat(row, num);
+        try {
+            if (reservation.makeReservation(seat, name)) {
+                response = Response.ok().build();
+            } else {
+                response = Response.status(Response.Status.NOT_ACCEPTABLE).build();
+            }
+        } catch (InvalidSeatException e) {
+            response = Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return response; // TODO: Create response
     }
 }
